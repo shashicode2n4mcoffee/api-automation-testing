01cc43d8b7bbaa8732a703dbb428579c9977c810
shashicode2n4mcoffee <shashicode2n4mcoffee@gmail.com>
2023-05-08 11:54:06 +0530

    COmpleted adding all git api tests

---
+++ configs/endpoints.js
@@ -0,0 +1,5 @@
+export const ENDPOINTS ={
+    BASE_URL:'https://gorest.co.in/public/v2',
+    USERS_BASE:'/users',
+    USERS : '/users?access-token='
+} 
--- test/users.js
+++ test/users.js
@@ -1,22 +1,32 @@
 import supertest from "supertest";
-
-const request = supertest('https://gorest.co.in/public/v2')
-
 import { expect } from "chai";
 
-const TOKEN = 'ee25f68e742989d5b7b2bc11f20a768a5101520ead1f1d3b3070ba01e71afead';
+import { ENDPOINTS } from "../configs/endpoints";
+
+require('dotenv').config()
+
+const request = supertest(ENDPOINTS.BASE_URL)
 
 describe('Users',()=>{
     it('GET /users', (done)=>{
-        request.get(`/users?access-token=${TOKEN}`).end((err,res)=>{
+        request.get(`${ENDPOINTS.USERS}${process.env.TOKEN}`).end((err,res)=>{
             expect(res.body).to.not.be.empty;
             done()
         })
     })
 
     it('GET /users/:id',()=>{
-        return request.get(`/users/1440235?access-token=${TOKEN}`).then((res)=>{
+        return request.get(`${ENDPOINTS.USERS_BASE}/1440235?access-token=${process.env.TOKEN}`).then((res)=>{
             expect(res.body.id).to.be.equal(1440235)
+        })
+    })
+
+    it('GET /users with the query params',()=>{
+        return request.get(`${ENDPOINTS.USERS}${process.env.TOKEN}&page=100&per_page=10&gender=female`).then(res=>{
+            expect(res.body).to.be.not.empty;
+            res.body.forEach(element => {
+                expect(element.gender).to.be.equal('female')
+            });
         })
     })
 })
